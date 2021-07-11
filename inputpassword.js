6aea716813d3864f0132e8b82436015bd0654916
prantl81 <33022108+prantl81@users.noreply.github.com>
2021-07-11 15:02:27 +0200

    Update inputpassword.js

--- inputpassword.js
+++ inputpassword.js
@@ -23,8 +23,7 @@
 		<TreeTable
                     id="TreeTableBasic"
                     rows="
-      "categories": [
-        {"name": "Women", "categories": [
+      {"name": "Women", "categories": [
           {"name":"Clothing", "categories": [
             {"name": "Dresses", "categories": [
               {"name": "Casual Red Dress", "amount": 16.99, "currency": "EUR", "size": "S"},
@@ -46,20 +45,6 @@
               {"name": "Black Skirt", "amount": 44.99, "currency": "USD", "size": "S"}
             ]}
           ]},
-          {"name":"Jewelry", "categories": [
-              {"name": "Necklace", "amount": 16.99, "currency": "USD"},
-              {"name": "Bracelet", "amount": 47.99, "currency": "USD"},
-              {"name": "Gold Ring", "amount": 399.99, "currency": "USD"}
-            ]},
-          {"name":"Handbags", "categories": [
-            {"name": "Little Black Bag", "amount": 16.99, "currency": "USD", "size": "S"},
-            {"name": "Grey Shopper", "amount": 47.99, "currency": "USD", "size": "M"}
-          ]},
-          {"name":"Shoes", "categories": [
-            {"name": "Pumps", "amount": 89.99, "currency": "USD"},
-            {"name": "Sport Shoes", "amount": 47.99, "currency": "USD"},
-            {"name": "Boots", "amount": 103.99, "currency": "USD"}
-          ]}
         ]},
         {"name": "Men", "categories": [
           {"name":"Clothing", "categories": [
@@ -82,69 +67,8 @@
             {"name": "Wallet", "amount": 47.99, "currency": "USD"},
             {"name": "Sunglasses", "amount": 199.99, "currency": "USD"}
           ]},
-          {"name":"Shoes", "categories": [
-            {"name": "Fashion Sneaker", "amount": 89.99, "currency": "USD"},
-            {"name": "Sport Shoe", "amount": 47.99, "currency": "USD"},
-            {"name": "Boots", "amount": 103.99, "currency": "USD"}
-          ]}
-        ]},
-          {"name": "Girls", "categories": [
-            {"name":"Clothing", "categories": [
-              {"name": "Shirts", "categories": [
-                {"name": "Red T-shirt", "amount": 16.99, "currency": "USD", "size": "S"},
-                {"name": "Tunic Top", "amount": 47.99, "currency": "USD", "size": "M"},
-                {"name": "Fuzzy Sweater", "amount": 103.99, "currency": "USD", "size": "L"}
-              ]},
-              {"name": "Pants", "categories": [
-                {"name": "Blue Jeans", "amount": 24.99, "currency": "USD", "size": "M"},
-                {"name": "Red Pant", "amount": 54.99, "currency": "USD", "size": "S"}
-              ]},
-              {"name": "Shorts", "categories": [
-                {"name": "Jeans Short", "amount": 32.99, "currency": "USD", "size": "M"},
-                {"name": "Sport Short", "amount": 14.99, "currency": "USD", "size": "S"}
-              ]}
-            ]},
-            {"name":"Accessories", "categories": [
-              {"name": "Necklace", "amount": 26.99, "currency": "USD"},
-              {"name": "Gloves", "amount": 7.99, "currency": "USD"},
-              {"name": "Beanie", "amount": 12.99, "currency": "USD"}
-            ]},
-            {"name":"Shoes", "categories": [
-              {"name": "Sport Shoes", "amount": 39.99, "currency": "USD"},
-              {"name": "Boots", "amount": 87.99, "currency": "USD"},
-              {"name": "Sandals", "amount": 63.99, "currency": "USD"}
             ]}
-          ]},
-            {"name": "Boys", "categories": [
-              {"name":"Clothing", "categories": [
-                {"name": "Shirts", "categories": [
-                  {"name": "Black T-shirt with Print", "amount": 16.99, "currency": "USD", "size": "S"},
-                  {"name": "Blue Shirt", "amount": 47.99, "currency": "USD", "size": "M"},
-                  {"name": "Yellow Sweater", "amount": 63.99, "currency": "USD", "size": "L"}
-                ]},
-                {"name": "Pants", "categories": [
-                  {"name": "Blue Jeans", "amount": 44.99, "currency": "USD", "size": "M"},
-                  {"name": "Brown Pant", "amount": 89.99, "currency": "USD", "size": "S"}
-                ]},
-                {"name": "Shorts", "categories": [
-                  {"name": "Sport Short", "amount": 32.99, "currency": "USD", "size": "M"},
-                  {"name": "Jeans Short", "amount": 99.99, "currency": "USD", "size": "XS"},
-                  {"name": "Black Short", "amount": 56.99, "currency": "USD", "size": "L"}
-                ]}
-              ]},
-              {"name":"Accessories", "categories": [
-                {"name": "Sunglasses", "amount": 36.99, "currency": "USD"},
-                {"name": "Beanie", "amount": 17.99, "currency": "USD"},
-                {"name": "Scarf", "amount": 15.99, "currency": "USD"}
-              ]},
-              {"name":"Shoes", "categories": [
-                {"name": "Sneaker", "amount": 89.99, "currency": "USD"},
-                {"name": "Sport Shoe", "amount": 47.99, "currency": "USD"},
-                {"name": "Boots", "amount": 103.99, "currency": "USD"}
-              ]}
-            ]}
-        ]}
-    }
+       }
 		    
 		    , parameters: {arrayNames:['categories']}}"
                     selectionMode="MultiToggle"
